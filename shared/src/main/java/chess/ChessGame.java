5e5be62c944ae243e89da239e84fafab4c20f1cd
perryvw12 <perry.vw12@gmail.com>
2024-09-09 12:01:02 -0600

    updated how turns are kept track of in ChessGame.java

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -9,6 +9,7 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
+    TeamColor turn = TeamColor.WHITE;
 
     public ChessGame() {
 
@@ -18,7 +19,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return turn;
     }
 
     /**
@@ -27,7 +28,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        turn = (turn == TeamColor.WHITE) ? (TeamColor.BLACK) : (TeamColor.WHITE);
     }
 
     /**
