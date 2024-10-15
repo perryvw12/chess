66e610e4d5abe101bdc5280e3a8eb3c08f6833d3
perryvw12 <perry.vw12@gmail.com>
2024-10-14 20:44:45 -0600

    Added some methods to the UserDataAccess interface.

--- server/src/main/java/dataaccess/UserDataAccess.java
+++ server/src/main/java/dataaccess/UserDataAccess.java
@@ -1,4 +1,8 @@
 package dataaccess;
+import model.UserData;
 
 public interface UserDataAccess {
+    UserData createUser(UserData user) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
 }
