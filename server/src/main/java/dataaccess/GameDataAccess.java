fea421098a9acdef682798f24c811a6b545dd833
perryvw12 <perry.vw12@gmail.com>
2024-10-14 20:51:27 -0600

    Added some methods to the GameDataAccess interface.

--- server/src/main/java/dataaccess/GameDataAccess.java
+++ server/src/main/java/dataaccess/GameDataAccess.java
@@ -1,4 +1,14 @@
 package dataaccess;
+import chess.ChessGame;
+import model.GameData;
+import java.util.ArrayList;
 
 public interface GameDataAccess {
+    String createGame(GameData gameData) throws DataAccessException;
+
+    GameData getGame(String gameID) throws DataAccessException;
+
+    ArrayList<GameData> listGames() throws DataAccessException;
+
+    boolean updateGame(String gameID, ChessGame game) throws DataAccessException;
 }
