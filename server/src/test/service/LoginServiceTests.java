83e96df13aa94e411cf8fdac92c175c3b2bf7e87
perryvw12 <perry.vw12@gmail.com>
2024-10-20 16:36:25 -0600

    created good and bad test cases for login service. passes all tests.

--- server/src/test/service/LoginServiceTests.java
+++ server/src/test/service/LoginServiceTests.java
@@ -14,4 +14,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginServiceTests {
     static DataAccess dataAccess = new DataAccess(DataAccess.Implementation.MEMORY);
     static LoginService loginService = new LoginService(dataAccess);
+
+    @BeforeAll
+    static void addUserTestData() throws DataAccessException {
+        var testUser = new UserData("testUser", "password", "g@mail");
+        dataAccess.userDataAccess.createUser(testUser);
+    }
+
+    @Test
+    void successfulLogin() throws ServiceException, DataAccessException {
+        var expectedAuthData = loginService.loginUser("testUser", "password");
+        var actualAuthData = dataAccess.authDataAccess.getAuth(expectedAuthData.authToken());
+        assertEquals(expectedAuthData, actualAuthData);
+    }
+
+    @Test
+    void badPassword() {
+        assertThrows(ServiceException.class, () ->
+                loginService.loginUser("testUser", "badPass"));
+    }
+
 }
