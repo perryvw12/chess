591e7a143130a2d5383baec7108fc929e77e76e8
perryvw12 <perry.vw12@gmail.com>
2024-10-21 19:27:12 -0600

    fixed file structure

--- server/src/test/java/service/JoinGameServiceTests.java
+++ server/src/test/java/service/JoinGameServiceTests.java
@@ -2,9 +2,7 @@ package service;
 
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
-import model.UserData;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
