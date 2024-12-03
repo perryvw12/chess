6cbbed485cc81228f7b38a4fcb97d959703618d9
perryvw12 <perry.vw12@gmail.com>
2024-12-03 13:31:15 -0700

    updated leave in the WebsocketHandler to update the game state to remove the username from black or white player if applicable.

--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -1,13 +1,16 @@
 package server.websocket;
 
+import chess.ChessGame;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import exception.ServiceException;
+import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import websocket.commands.UserGameCommand;
 import com.google.gson.Gson;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
@@ -39,8 +42,13 @@ public class WebsocketHandler {
        }
     }
 
-    private void connect(String username, Session session, Integer gameID) throws IOException {
+    private void connect(String username, Session session, Integer gameID) throws IOException, ServiceException, DataAccessException {
         connections.saveSession(username, session, gameID);
+
+        ChessGame game = (dataAccess.gameDataAccess.getGame(gameID)).chessGame();
+        var loadGameMessage = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
+        connections.broadcast(null, gameID, loadGameMessage);
+
         var message = String.format("%s has joined the game", username);
         var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
         connections.broadcast(username, gameID, notification);
@@ -50,8 +58,18 @@ public class WebsocketHandler {
 
     }
 
-    private void leave(String username, Integer gameID) throws IOException {
+    private void leave(String username, Integer gameID) throws IOException, ServiceException, DataAccessException {
         connections.deleteSession(username);
+
+        GameData gameData = dataAccess.gameDataAccess.getGame(gameID);
+        if(gameData.whiteUsername().equals(username)) {
+            GameData updatedGame = new GameData(gameID,null, gameData.blackUsername(), gameData.gameName(), gameData.chessGame());
+            dataAccess.gameDataAccess.updateGame(gameID, updatedGame);
+        } else if (gameData.blackUsername().equals(username)) {
+            GameData updatedGame = new GameData(gameID, gameData.whiteUsername(), null, gameData.gameName(), gameData.chessGame());
+            dataAccess.gameDataAccess.updateGame(gameID, updatedGame);
+        }
+
         var message = String.format("%s has left the game", username);
         var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
         connections.broadcast(username, gameID, notification);
