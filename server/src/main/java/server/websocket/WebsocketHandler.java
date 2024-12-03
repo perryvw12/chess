b75a7217a7a9c3d3e59b5a497a2bacabf963ac2c
perryvw12 <perry.vw12@gmail.com>
2024-12-03 13:53:35 -0700

    implemented the make move command on the server side inside of the WebsocketHandler

--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -1,6 +1,8 @@
 package server.websocket;
 
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.InvalidMoveException;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import exception.ServiceException;
@@ -8,6 +10,7 @@ import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import com.google.gson.Gson;
 import websocket.messages.LoadGameMessage;
@@ -33,7 +36,7 @@ public class WebsocketHandler {
 
            switch (command.getCommandType()) {
                case CONNECT -> connect(username, session, gameID);
-               case MAKE_MOVE -> makeMove();
+               case MAKE_MOVE -> makeMove(command, username, gameID);
                case LEAVE -> leave(username, gameID);
                case RESIGN -> resign();
            }
@@ -54,8 +57,41 @@ public class WebsocketHandler {
         connections.broadcast(username, gameID, notification);
     }
 
-    private void makeMove() {
+    private void makeMove(UserGameCommand command, String username, Integer gameID) throws ServiceException, DataAccessException, IOException {
+        ChessMove chessMove = ((MakeMoveCommand) command).getChessMove();
+        GameData gameData = dataAccess.gameDataAccess.getGame(gameID);
+        ChessGame game = gameData.chessGame();
 
+        try {
+            game.makeMove(chessMove);
+        } catch (InvalidMoveException e) {
+            throw new RuntimeException(e);
+        }
+        GameData updatedGame = new GameData(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
+        dataAccess.gameDataAccess.updateGame(gameID, updatedGame);
+
+        var loadGameMessage = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, game);
+        connections.broadcast(null, gameID, loadGameMessage);
+
+        var moveMessage = String.format("piece was moved from %s to %s", chessMove.getStartPosition(), chessMove.getEndPosition());
+        var moveNotification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, moveMessage);
+        connections.broadcast(username, gameID, moveNotification);
+
+        String statusMessage = null;
+        if(game.isInCheckmate(ChessGame.TeamColor.BLACK)) {
+            statusMessage = String.format("%s has been checkmated", gameData.blackUsername());
+        } else if (game.isInCheckmate(ChessGame.TeamColor.WHITE)) {
+            statusMessage = String.format("%s has been checkmated", gameData.whiteUsername());
+        } else if (game.isInCheck(ChessGame.TeamColor.BLACK)) {
+            statusMessage = String.format("%s is in check", gameData.blackUsername());
+        } else if (game.isInCheck(ChessGame.TeamColor.WHITE)) {
+            statusMessage = String.format("%s is in check", gameData.whiteUsername());
+        }
+
+        if(statusMessage != null) {
+            var statusNotification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, statusMessage);
+            connections.broadcast(null, gameID, statusNotification);
+        }
     }
 
     private void leave(String username, Integer gameID) throws IOException, ServiceException, DataAccessException {
