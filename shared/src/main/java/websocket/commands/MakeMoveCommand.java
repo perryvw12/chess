c173c61c7ff4b2a60ceb38c5e97c7845dfe5460c
perryvw12 <perry.vw12@gmail.com>
2024-12-03 13:08:50 -0700

    Created child class that extends UserGameCommand for the optional ChessMove parameter.

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,15 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+
+public class MakeMoveCommand extends UserGameCommand{
+    private final ChessMove chessMove;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove chessMove) {
+        super(commandType, authToken, gameID);
+        this.chessMove = chessMove;
+    }
+
+    public ChessMove getChessMove() {return chessMove;}
+}
