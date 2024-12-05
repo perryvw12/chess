4cd2c8c11a3cdb937d1e78923fcf8a4125c8edf7
perryvw12 <perry.vw12@gmail.com>
2024-12-04 21:22:14 -0700

    code quality duplication within the board drawer

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -11,18 +11,13 @@ public class BoardDrawer {
     public static String drawBoard(ChessGame game, ChessPosition chessPosition, boolean isWhitePerspective) {
         ChessBoard board = game.getBoard();
         Collection<ChessMove> validMoves = null;
-        if(chessPosition != null) {
+        if (chessPosition != null) {
             validMoves = game.validMoves(chessPosition);
         }
 
         StringBuilder drawBoard = new StringBuilder(EscapeSequences.ERASE_SCREEN);
 
-        String letters = isWhitePerspective
-                ? String.format("%sa  \u2009b  \u2009\u2009c  \u2009\u2009d  \u2009\u2009e  " +
-                "\u2009\u2009f  \u2009\u2009g  \u2009\u2009h %s\n", EscapeSequences.EMPTY, EscapeSequences.EMPTY)
-                : String.format("%sh  \u2009g  \u2009\u2009f  \u2009\u2009e  \u2009\u2009d  \u2009\u2009c  " +
-                "\u2009\u2009b  \u2009\u2009a %s\n", EscapeSequences.EMPTY, EscapeSequences.EMPTY);
-
+        String letters = getColumnLabels(isWhitePerspective);
         drawBoard.append(letters);
 
         int startRow = isWhitePerspective ? 8 : 1;
@@ -31,54 +26,48 @@ public class BoardDrawer {
         for (int i = 0; i < 8; i++) {
             int rowLabel = startRow + rowIncrement * i;
             drawBoard.append(rowLabel).append(" ");
-
-            if(isWhitePerspective) {
-                for (int col = 0; col < 8; col++) {
-                    String bgColor = ((i + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY : EscapeSequences.SET_BG_COLOR_DARK_GREY;
-                    ChessPosition position = new ChessPosition(rowLabel, col + 1);
-                    if (validMoves != null) {
-                        for (ChessMove move : validMoves) {
-                            if (position.equals(move.getEndPosition())) {
-                                bgColor = ((i + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_GREEN : EscapeSequences.SET_BG_COLOR_DARK_GREEN;
-                                break;
-                            }
-                        }
-                        if (position.equals(chessPosition)) {
-                            bgColor = EscapeSequences.SET_BG_COLOR_HIGHLIGHT;
-                        }
-                    }
-                    ChessPiece piece = board.getPiece(position);
-                    String pieceSymbol = (piece != null) ? getPieceSymbol(piece) : EscapeSequences.EMPTY;
-
-                    drawBoard.append(bgColor).append(pieceSymbol).append(EscapeSequences.RESET_BG_COLOR);
-                }
-            } else {
-                for (int col = 8; col > 0; col--) {
-                    String bgColor = ((i + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY : EscapeSequences.SET_BG_COLOR_DARK_GREY;
-                    ChessPosition position = new ChessPosition(rowLabel, col);
-                    if (validMoves != null) {
-                        for (ChessMove move : validMoves) {
-                            if (position.equals(move.getEndPosition())) {
-                                bgColor = ((i + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_GREEN : EscapeSequences.SET_BG_COLOR_DARK_GREEN;
-                                break;
-                            }
-                        }
-                        if (position.equals(chessPosition)) {
-                            bgColor = EscapeSequences.SET_BG_COLOR_HIGHLIGHT;
-                        }
-                    }
-                    ChessPiece piece = board.getPiece(position);
-                    String pieceSymbol = (piece != null) ? getPieceSymbol(piece) : EscapeSequences.EMPTY;
-
-                    drawBoard.append(bgColor).append(pieceSymbol).append(EscapeSequences.RESET_BG_COLOR);
-                }
-            }
-
+            drawRow(drawBoard, board, validMoves, chessPosition, rowLabel, isWhitePerspective);
             drawBoard.append(" ").append(rowLabel).append("\n");
         }
 
         drawBoard.append(letters);
         return drawBoard.toString();
+    }
+
+    private static void drawRow(StringBuilder drawBoard, ChessBoard board, Collection<ChessMove> validMoves,
+                                ChessPosition chessPosition, int rowLabel, boolean isWhitePerspective) {
+        int startCol = isWhitePerspective ? 1 : 8;
+        int colIncrement = isWhitePerspective ? 1 : -1;
+
+        for (int col = startCol; (isWhitePerspective ? col <= 8 : col >= 1); col += colIncrement) {
+            String bgColor = ((rowLabel + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY : EscapeSequences.SET_BG_COLOR_DARK_GREY;
+            ChessPosition position = new ChessPosition(rowLabel, col);
+
+            if (validMoves != null) {
+                for (ChessMove move : validMoves) {
+                    if (position.equals(move.getEndPosition())) {
+                        bgColor = ((rowLabel + col) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_GREEN : EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+                        break;
+                    }
+                }
+                if (position.equals(chessPosition)) {
+                    bgColor = EscapeSequences.SET_BG_COLOR_HIGHLIGHT;
+                }
+            }
+
+            ChessPiece piece = board.getPiece(position);
+            String pieceSymbol = (piece != null) ? getPieceSymbol(piece) : EscapeSequences.EMPTY;
+
+            drawBoard.append(bgColor).append(pieceSymbol).append(EscapeSequences.RESET_BG_COLOR);
+        }
+    }
+
+    private static String getColumnLabels(boolean isWhitePerspective) {
+        return isWhitePerspective
+                ? String.format("%sa  \u2009b  \u2009\u2009c  \u2009\u2009d  \u2009\u2009e  " +
+                "\u2009\u2009f  \u2009\u2009g  \u2009\u2009h %s\n", EscapeSequences.EMPTY, EscapeSequences.EMPTY)
+                : String.format("%sh  \u2009g  \u2009\u2009f  \u2009\u2009e  \u2009\u2009d  \u2009\u2009c  " +
+                "\u2009\u2009b  \u2009\u2009a %s\n", EscapeSequences.EMPTY, EscapeSequences.EMPTY);
     }
 
     private static String getPieceSymbol(ChessPiece piece) {
@@ -111,5 +100,3 @@ public class BoardDrawer {
         return drawBoard(game, chessPosition, false);
     }
 }
-
-
